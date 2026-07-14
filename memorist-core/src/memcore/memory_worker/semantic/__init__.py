72fc77085a2f22231d727dc8b1fdac75ed05ace6
H123 <cna.farokhi@gmail.com>
2026-07-14 10:48:11 +0330

    PR4-D Task 02: add semantic package exports

---
+++ memorist-core/src/memcore/memory_worker/semantic/__init__.py
@@ -0,0 +1,17 @@
+from memcore.memory_worker.semantic.canonical import (
+    CanonicalGateDecision,
+    CanonicalLinguisticComplements,
+    CanonicalRouteDecision,
+    CanonicalSemanticDecision,
+    CanonicalSemanticFactor,
+    CanonicalSemanticProvenance,
+)
+
+__all__ = [
+    "CanonicalGateDecision",
+    "CanonicalLinguisticComplements",
+    "CanonicalRouteDecision",
+    "CanonicalSemanticDecision",
+    "CanonicalSemanticFactor",
+    "CanonicalSemanticProvenance",
+]
