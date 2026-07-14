6cf92ac83a135ef795a03f450f20cc82bc9b42f3
H123 <cna.farokhi@gmail.com>
2026-07-14 15:28:45 +0330

    PR4-D Task 03: wire Full fallback resolver

--- memorist-core/src/memcore/memory_worker/postgres/__init__.py
+++ memorist-core/src/memcore/memory_worker/postgres/__init__.py
@@ -1,3 +1,12 @@
+from memcore.memory_worker.postgres.deterministic_fallback import (
+    deterministic_jakobson_output,
+)
 from memcore.memory_worker.postgres.pipeline import PostgresMemoryWorkerPipeline
+
+setattr(
+    PostgresMemoryWorkerPipeline,
+    "_deterministic_jakobson_output",
+    deterministic_jakobson_output,
+)
 
 __all__ = ["PostgresMemoryWorkerPipeline"]
