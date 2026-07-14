3c29073fcff29f15bb36d9c49fbab23c875a51f9
H123 <cna.farokhi@gmail.com>
2026-07-15 01:05:31 +0330

    feat: export route candidate mapping contract

--- memorist-core/src/memcore/memory_worker/semantic/__init__.py
+++ memorist-core/src/memcore/memory_worker/semantic/__init__.py
@@ -1,3 +1,8 @@
+from memcore.memory_worker.semantic.candidate_mapping import (
+    ROUTE_CANDIDATE_MAPPING_VERSION,
+    RouteCandidateMapping,
+    candidate_mapping_for_route,
+)
 from memcore.memory_worker.semantic.canonical import (
     CanonicalGateDecision,
     CanonicalLinguisticComplements,
@@ -35,9 +40,12 @@ __all__ = [
     "CanonicalSemanticProvenance",
     "ContextKind",
     "GateCandidatePolicy",
+    "ROUTE_CANDIDATE_MAPPING_VERSION",
     "ReceiverKind",
     "ResolvedSemanticFactors",
+    "RouteCandidateMapping",
     "SemanticFactorMatch",
+    "candidate_mapping_for_route",
     "candidate_policy_for_gate_and_route",
     "decide_semantic_routes",
     "gate_allows_analysis",
