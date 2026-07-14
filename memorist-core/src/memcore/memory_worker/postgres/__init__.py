187514a35b56381077dca5df73c28a17f6b08fd2
H123 <cna.farokhi@gmail.com>
2026-07-14 15:41:24 +0330

    Wire Full routes through shared policy

--- memorist-core/src/memcore/memory_worker/postgres/__init__.py
+++ memorist-core/src/memcore/memory_worker/postgres/__init__.py
@@ -2,11 +2,13 @@ from memcore.memory_worker.postgres.deterministic_fallback import (
     deterministic_jakobson_output,
 )
 from memcore.memory_worker.postgres.pipeline import PostgresMemoryWorkerPipeline
+from memcore.memory_worker.postgres.routing_policy_adapter import record_routes
 
 setattr(
     PostgresMemoryWorkerPipeline,
     "_deterministic_jakobson_output",
     deterministic_jakobson_output,
 )
+setattr(PostgresMemoryWorkerPipeline, "_record_routes", record_routes)
 
 __all__ = ["PostgresMemoryWorkerPipeline"]
